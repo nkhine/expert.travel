7e7c8ea846af14c724f9905ee82ed87706037414
Norman Khine <khinester@khinester.(none)>
2007-07-05 18:52:44 +0200

    [JOBS SKIN] Working on the jobs views.
    Removed the table object from the job view page
    as this does not look too nice. Also reduced the
    Job Description text.
    
    Now the batch is also specific to the view, instead
    of calling everything 'objects'
    
    Added the Commission Junction advertisements.

--- uktravel.py
+++ uktravel.py
@@ -5,15 +5,16 @@
 import datetime
 
 # Import from itools
-from itools.stl import stl
-from itools.cms.registry import register_object_class
-from itools.cms.html import XHTMLFile
 from itools.catalog import EqQuery, AndQuery, PhraseQuery, RangeQuery
+from itools.cms.html import XHTMLFile
+from itools.cms.registry import register_object_class
 from itools.cms.widgets import table, batch
+from itools.cms.utils import reduce_string
+from itools.stl import stl
 
 # Import from abakuc
-from website import WebSite
 from metadata import JobTitle, SalaryRange
+from website import WebSite
 
 
 class UKTravel(WebSite):
@@ -67,7 +68,7 @@ class UKTravel(WebSite):
         raise KeyError
 
     #######################################################################
-    #
+    # List last 5 jobs for Home page
     #######################################################################
     def view(self, context):
         root = context.root
@@ -104,20 +105,34 @@ class UKTravel(WebSite):
     #######################################################################
     view_jobs__access__ = True
     view_jobs__label__ = u'Our jobs'
-    def view_jobs(self, context):
+    def view_jobs(self, context, word_treshold=30):
         namespace = {}
         namespace['batch'] = ''
-        columns = [('title', u'Title'),
-                   ('closing_date', u'Closing Date'),
-                   ('company', u'Company'),
-                   ('function', u'Function'),
-                   ('description', u'Short description')]
+        #columns = [('title', u'Title'),
+        #           ('description', u'Short description'),
+        #           ('company', u'Company'),
+        #           ('function', u'Function'),
+        #           ('closing_date', u'Closing Date')]
         
+        #Total number of jobs
+        #XXX Don't like this, as there are two catalogs now!!! 
+        root = context.root
+        howmany = context.server.catalog
+        number_of_jobs = []
+        number_of_jobs.append(EqQuery('format', 'Job'))
+        today = (datetime.date.today()).strftime('%Y-%m-%d')
+        number_of_jobs.append(RangeQuery('closing_date', today, None))
+        number_of_jobs = AndQuery(*number_of_jobs)
+        results = howmany.search(number_of_jobs)
+        number_of_jobs = results.get_documents()
+        namespace['number_of_jobs'] = len(number_of_jobs)
         # Search fields
+        search_text = context.get_form_value('search_text') or None
         function = context.get_form_value('function') or None
         salary = context.get_form_value('salary') or None
+        #XXX This does not work see bug #86
         job_title = context.get_form_value('job_title') or None
-        # Get Jobs (construct the query for the research)
+        # Get Jobs (construct the query for the search)
         root = context.root
         catalog = context.server.catalog
         query = []
@@ -131,65 +146,80 @@ class UKTravel(WebSite):
         query = AndQuery(*query)
         results = catalog.search(query)
         documents = results.get_documents()
+        namespace['nb_jobs'] = len(documents)
         # Construct the lines of the table
         add_line = True
         jobs = []
         for job in documents:
             job = root.get_handler(job.abspath)
             get = job.get_property
-            # If it's a search by Title , Check if we add the line
-            if job_title:
-                critere = job.get_property('dc:title')
-                add_line = False
-                job_title = job_title.lower()
-                if job_title in critere:
-                    add_line = True
-            if add_line:
-                # Informations about The company
-                address = job.parent
-                company = address.parent
-                # Information about the job
-                url = '/companies/%s/%s/%s/;view' % (company.name, address.name,
-                                                     job.name)
-                job_to_add ={'img': '/ui/abakuc/images/JobBoard16.png',
-                             'title': (get('dc:title'),url),
-                             'closing_date': get('abakuc:closing_date'),
-                             'company': company.get_property('dc:title'),
-                             'function': JobTitle.get_value(
-                                            get('abakuc:function')),
-                             'description': get('dc:description')}
-                jobs.append(job_to_add)
+            address = job.parent
+            company = address.parent
+            url = '/companies/%s/%s/%s' % (company.name, address.name, job.name)
+            description = reduce_string(get('dc:description'),
+                                             word_treshold=90,
+                                             phrase_treshold=240)
+            jobs.append({'url': url,
+                         'title': job.title,
+                         'description': description})
+            ## If it's a search by Title , Check if we add the line
+            #if job_title:
+            #    criteria = job.get_property('dc:title')
+            #    add_line = False
+            #    job_title = job_title.lower()
+            #    if job_title in criteria:
+            #        add_line = True
+            #if add_line:
+            #    # Informations about the company
+            #    address = job.parent
+            #    company = address.parent
+            #    # Information about the job
+            #    url = '/companies/%s/%s/%s/;view' % (company.name, address.name,
+            #                                         job.name)
+            #    job_to_add ={'img': '/ui/abakuc/images/JobBoard16.png',
+            #                 'title': (get('dc:title'),url),
+            #                 'closing_date': get('abakuc:closing_date'),
+            #                 'company': company.get_property('dc:title'),
+            #                 'function': JobTitle.get_value(
+            #                                get('abakuc:function')),
+            #                 'description': get('dc:description')}
+            #    jobs.append(job_to_add)
            
-        # Sort
-          # => XXX See if it's correct
-        sortby = context.get_form_value('sortby', 'title')
-        sortorder = context.get_form_value('sortorder', 'up')
-        reverse = (sortorder == 'down')
-        jobs.sort(lambda x,y: cmp(x[sortby], y[sortby]))
-        if reverse:
-            jobs.reverse()
+        ## Sort
+        #  # => XXX See if it's correct
+        #sortby = context.get_form_value('sortby', 'title')
+        #sortorder = context.get_form_value('sortorder', 'up')
+        #reverse = (sortorder == 'down')
+        #jobs.sort(lambda x,y: cmp(x[sortby], y[sortby]))
+        #if reverse:
+        #    jobs.reverse()
         # Set batch informations
         batch_start = int(context.get_form_value('batchstart', default=0))
-        batch_size = 20
+        batch_size = 5 
         batch_total = len(jobs)
         batch_fin = batch_start + batch_size
         if batch_fin > batch_total:
             batch_fin = batch_total
         jobs = jobs[batch_start:batch_fin]
-        # Table Namespace 
+        #Namespace 
         if jobs:
-            job_table = table(columns, jobs, [sortby], sortorder,[])
+            #job_table = table(columns, jobs, [sortby], sortorder,[])
             job_batch = batch(context.uri, batch_start, batch_size,
-                              batch_total)
+                              batch_total, 
+                              msgs=(u"There is 1 job announcement.",
+                                    u"There are ${n} job announcements."))
             msg = None
         else:
-            job_table = None
+            #job_table = None
             job_batch = None
-            msg = u'Sorry but there are no jobs'
-        namespace['table'] = job_table
+            msg = u"Appologies, currently we don't have any job announcements"
+        #namespace['table'] = job_table
         namespace['batch'] = job_batch
         namespace['msg'] = msg 
-        
+        namespace['jobs'] = jobs
+
+
+
         # Search Namespace
         namespace['function'] = JobTitle.get_namespace(function)
         namespace['salary'] = SalaryRange.get_namespace(salary)
