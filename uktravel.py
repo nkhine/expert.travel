8bbaf5e2344828d93e73f297e84d968e3ecb4fb8
J. David Ibanez <jdavid@itaapy.com>
2007-07-17 19:15:17 +0200

    Clean "view_jobs" code before fixing bug 86.

--- uktravel.py
+++ uktravel.py
@@ -2,7 +2,7 @@
 # Copyright (C) 2007 Norman Khine <norman@abakuc.com>
 
 # Import from the standard library
-import datetime
+from datetime import date
 
 # Import from itools
 from itools.catalog import EqQuery, AndQuery, PhraseQuery, RangeQuery
@@ -77,7 +77,7 @@ class UKTravel(WebSite):
         catalog = context.server.catalog
         query = []
         query.append(EqQuery('format', 'Job'))
-        today = (datetime.date.today()).strftime('%Y-%m-%d')
+        today = date.today().strftime('%Y-%m-%d')
         query.append(RangeQuery('closing_date', today, None))
         query = AndQuery(*query)
         results = catalog.search(query)
@@ -106,51 +106,33 @@ class UKTravel(WebSite):
     view_jobs__access__ = True
     view_jobs__label__ = u'Our jobs'
     def view_jobs(self, context):
-        namespace = {}
-        namespace['batch'] = ''
-        #columns = [('title', u'Title'),
-        #           ('description', u'Short description'),
-        #           ('company', u'Company'),
-        #           ('function', u'Function'),
-        #           ('closing_date', u'Closing Date')]
-        
-        #Total number of jobs
-        #XXX Don't like this, as there are two catalogs now!!! 
         root = context.root
-        howmany = context.server.catalog
-        number_of_jobs = []
-        number_of_jobs.append(EqQuery('format', 'Job'))
-        today = (datetime.date.today()).strftime('%Y-%m-%d')
-        number_of_jobs.append(RangeQuery('closing_date', today, None))
-        number_of_jobs = AndQuery(*number_of_jobs)
-        results = howmany.search(number_of_jobs)
-        number_of_jobs = results.get_documents()
-        namespace['number_of_jobs'] = len(number_of_jobs)
+
+        namespace = {}
+        # Total number of jobs
+        today = date.today().strftime('%Y-%m-%d')
+        query = [EqQuery('format', 'Job'),
+                 RangeQuery('closing_date', today, None)]
+        results = root.search(AndQuery(*query))
+        namespace['number_of_jobs'] = results.get_n_documents()
+
         # Search fields
-        search_text = context.get_form_value('search_text') or None
         function = context.get_form_value('function') or None
         salary = context.get_form_value('salary') or None
-        #XXX This does not work see bug #86
+        # XXX This does not work see bug #86
         job_title = context.get_form_value('job_title') or None
         # Get Jobs (construct the query for the search)
-        root = context.root
-        catalog = context.server.catalog
-        query = []
-        query.append(EqQuery('format', 'Job'))
-        today = (datetime.date.today()).strftime('%Y-%m-%d')
-        query.append(RangeQuery('closing_date', today, None))
         if function:
             query.append(EqQuery('function', function))
         if salary:
             query.append(EqQuery('salary', salary))
-        query = AndQuery(*query)
-        results = catalog.search(query)
-        documents = results.get_documents()
-        namespace['nb_jobs'] = len(documents)
+        results = root.search(AndQuery(*query))
+        namespace['nb_jobs'] = results.get_n_documents()
+
         # Construct the lines of the table
         add_line = True
         jobs = []
-        for job in documents:
+        for job in results.get_documents():
             job = root.get_handler(job.abspath)
             get = job.get_property
             address = job.parent
@@ -160,47 +142,15 @@ class UKTravel(WebSite):
             salary = get('abakuc:salary')
             closing_date = get('abakuc:closing_date')
             description = reduce_string(get('dc:description'),
-                                             word_treshold=90,
-                                             phrase_treshold=240)
-            jobs.append({'url': url,
-                         'title': job.title,
-                         'function': JobTitle.get_value(
-                                        get('abakuc:function')),
-                         'salary': SalaryRange.get_value(
-                                        get('abakuc:salary')),
-                         'closing_date': get('abakuc:closing_date'),
-                         'description': description})
-            ## If it's a search by Title , Check if we add the line
-            #if job_title:
-            #    criteria = job.get_property('dc:title')
-            #    add_line = False
-            #    job_title = job_title.lower()
-            #    if job_title in criteria:
-            #        add_line = True
-            #if add_line:
-            #    # Informations about the company
-            #    address = job.parent
-            #    company = address.parent
-            #    # Information about the job
-            #    url = '/companies/%s/%s/%s/;view' % (company.name, address.name,
-            #                                         job.name)
-            #    job_to_add ={'img': '/ui/abakuc/images/JobBoard16.png',
-            #                 'title': (get('dc:title'),url),
-            #                 'closing_date': get('abakuc:closing_date'),
-            #                 'company': company.get_property('dc:title'),
-            #                 'function': JobTitle.get_value(
-            #                                get('abakuc:function')),
-            #                 'description': get('dc:description')}
-            #    jobs.append(job_to_add)
-           
-        ## Sort
-        #  # => XXX See if it's correct
-        #sortby = context.get_form_value('sortby', 'title')
-        #sortorder = context.get_form_value('sortorder', 'up')
-        #reverse = (sortorder == 'down')
-        #jobs.sort(lambda x,y: cmp(x[sortby], y[sortby]))
-        #if reverse:
-        #    jobs.reverse()
+                                        word_treshold=90,
+                                        phrase_treshold=240)
+            jobs.append({
+                'url': url,
+                'title': job.title,
+                'function': JobTitle.get_value(get('abakuc:function')),
+                'salary': SalaryRange.get_value(get('abakuc:salary')),
+                'closing_date': get('abakuc:closing_date'),
+                'description': description})
         # Set batch informations
         batch_start = int(context.get_form_value('batchstart', default=0))
         batch_size = 5 
@@ -209,24 +159,19 @@ class UKTravel(WebSite):
         if batch_fin > batch_total:
             batch_fin = batch_total
         jobs = jobs[batch_start:batch_fin]
-        #Namespace 
+        # Namespace 
         if jobs:
-            #job_table = table(columns, jobs, [sortby], sortorder,[])
             job_batch = batch(context.uri, batch_start, batch_size,
                               batch_total, 
                               msgs=(u"There is 1 job announcement.",
                                     u"There are ${n} job announcements."))
             msg = None
         else:
-            #job_table = None
             job_batch = None
             msg = u"Appologies, currently we don't have any job announcements"
-        #namespace['table'] = job_table
         namespace['batch'] = job_batch
         namespace['msg'] = msg 
         namespace['jobs'] = jobs
-
-
 
         # Search Namespace
         namespace['function'] = JobTitle.get_namespace(function)
